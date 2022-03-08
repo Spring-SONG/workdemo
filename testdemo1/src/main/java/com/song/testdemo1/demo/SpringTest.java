cad2a442cc7ef53f0ed951522219a0a1961cc3c0
songshiqi <403395210@qq.com>
2022-03-08 16:24:28 +0800

    123

--- testdemo1/src/main/java/com/song/testdemo1/demo/SpringTest.java
+++ testdemo1/src/main/java/com/song/testdemo1/demo/SpringTest.java
@@ -1,9 +1,26 @@
 package com.song.testdemo1.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @Description:
  * @Author: songshiqi
  * @Date: 2022/3/7 14:37
  */
 public class SpringTest {
+    public static void main(String[] args) {
+        List<String> A =new ArrayList<>();
+        List<String> B =new ArrayList<>();
+        A.add("abc");
+        A.add("123");
+        B.add("def");
+//        B.stream().sequential().collect(Collectors.toCollection(() -> A));
+        A.addAll(B);
+        A.remove(0);
+        System.out.println(A.get(0));
+
+
+    }
 }
